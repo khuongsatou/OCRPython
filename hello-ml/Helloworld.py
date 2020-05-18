98e102ba3c0fc576a7b80f12ec9c7117f2bb677b
nvkhuong <vankhuong240499@gmail.com>
2020-05-18 22:16:22 +0700

    HelloWorld Machine Learning

--- hello-ml/Helloworld.py
+++ hello-ml/Helloworld.py
@@ -1,3 +1,10 @@
-import sklearn
-features = [[140,"smooth"],[130,"smooth"],[150,"bumpy"],[170,"bumpy"]]
-labels   = ["apple","apple","orange","orange"]
+from sklearn import tree
+#apple:0 =150,170 (bumpy)
+#orange:1 =140,130 (Smooth)
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels   = [0,0,1,1]
+
+clf = tree.DecisionTreeClassifier()
+clf =clf.fit(features,labels)
+print(clf.predict([[140,0]]))
+
